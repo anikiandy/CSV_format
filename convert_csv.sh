af5e6da96dfc15168330901292bfe44b9425a18b
anikiandy <ajshiraki@gmail.com>
2020-11-23 22:22:32 -0800

    whatever

--- convert_csv.sh
+++ convert_csv.sh
@@ -2,20 +2,20 @@
 count=`ls -1 *.CSV 2>/dev/null | wc -l`
 count2=`ls -1 *.csv 2>/dev/null | wc -l`
 if [ $count != 0 ]; then
-	
-	echo "converting Files"
-	for file in *.CSV
+	for file1 in *.CSV
 	do
 		if [ "$1" = "--overwrite" ]
 		then
-			sed 's/\t/,/g' "$file" > "temp";
-			mv temp "$file";
+			sed 's/\t/,/g' "$file1" > "temp";
+			mv temp "$file1";
 		else
-			sed 's/\t/,/g' "$file" > "c_${file}"
+			sed 's/\t/,/g' "$file1" > "c_${file1}"
 		fi
 	done
-elif [ $count2 != 0 ]; then
-	echo "converting .csv";
+else
+	echo "no .CSV files"
+fi
+if [ $count2 != 0 ]; then
 	for file in *.csv
 	do
 		if [ "$1" = "--overwrite" ]
@@ -26,6 +26,4 @@ elif [ $count2 != 0 ]; then
 			sed 's/\t/,/g' "$file" > "c_${file}"
 		fi
 	done
-else
-	echo"no matching files found"
 fi
